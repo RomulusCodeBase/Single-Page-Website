f1e3f333007530f85fbdc4d87a5fde89ec85b150
connor <connor.churchill@wordshop.co.uk>
2022-06-12 21:27:46 +0100

    added page changes

---
+++ Romulus/Static/Assets/js/content-loader/contact.js
@@ -0,0 +1,35 @@
+document.addEventListener('alpine:init', () => {
+    Alpine.data('homepage', () => ({
+        ContentBlocks: [
+
+        ],
+        ShowMenu: false,
+        ScrollToContent(){
+            var content = document.getElementById("contents-container");
+            content.scrollIntoView();
+        },
+        RenderComponents(){
+            axios.all([
+                 axios.get('/Assets/data/contents.json'), 
+                 axios.get('/Assets/data/contact.json')
+              ])
+              .then(axios.spread((ResponseOne, ResponseTwo) => {
+                var converter = new showdown.Converter();    
+                this.ContentBlocks.push(ResponseOne);
+                this.ContentBlocks.push(ResponseTwo);  
+                for(Panel in this.ContentBlocks[1].data.PanelList){
+                    var MarkdownTarget =  this.ContentBlocks[1].data.PanelList[Panel].PanelText;
+                    var RevisedHtml = converter.makeHtml(MarkdownTarget);
+                    this.ContentBlocks[1].data.PanelList[Panel].PanelText = RevisedHtml;
+                }
+            }));
+        },
+        ToggleMenu(x){
+            var menu = document.getElementById("menu");
+            var mainMenu = document.getElementById("main-menu");
+            menu.classList.toggle("change");
+            mainMenu.classList.toggle("hidden");
+            document.body.classList.toggle("overflow-hidden")
+        }
+    }))
+})
